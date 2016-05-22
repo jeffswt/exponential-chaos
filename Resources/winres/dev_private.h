a9480620844667ecab7898437acf6da75fe600bb
Geoffrey, Tang <jeffswt@outlook.com>
2016-05-22 22:32:04 +0800

    0.12.1 Release: development resources
    GUI mostly.

---
+++ Resources/winres/dev_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef DEV_PRIVATE_H
+#define DEV_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.12.1.0"
+#define VER_MAJOR	0
+#define VER_MINOR	12
+#define VER_RELEASE	1
+#define VER_BUILD	0
+#define COMPANY_NAME	""
+#define FILE_VERSION	"0.12.1.0"
+#define FILE_DESCRIPTION	"Exponential Chaos 0.12.1"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"Copyright (C) 2016, Geoffrey"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"Exponential Chaos 0.12.1"
+#define PRODUCT_VERSION	"0.12.1.0"
+
+#endif /*DEV_PRIVATE_H*/
