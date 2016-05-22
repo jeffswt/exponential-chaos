17cacb1a89130aa045ea45e2330c62edc2d11333
Geoffrey, Tang <jeffswt@outlook.com>
2016-05-22 22:13:34 +0800

    0.12.1 Release: native/inputctrl.h

---
+++ native/guid.h
@@ -0,0 +1,31 @@
+
+//	"guid.h" -*- C++ -*-
+//	Created on: 8 March 2016
+//	GUID generation and management.
+//
+//	Copyright (C) 2016  Geoffrey Tang.
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef NATIVE_GUID_H_
+#define NATIVE_GUID_H_
+
+#include "include/public.h"
+
+#include <cstdlib>
+
+long long	GenerateGuid(
+		void);
+
+#endif /* NATIVE_GUID_H_ */
---
+++ native/inputctrl.h
@@ -0,0 +1,43 @@
+
+//	"inputctrl.h" -*- C++ -*-
+//	Created on: 11 April 2016
+//	Input devices' main indexer.
+//
+//	Copyright (C) 2016  Geoffrey Tang.
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef NATIVE_INPUTCTRL_H_
+#define NATIVE_INPUTCTRL_H_
+
+#include "include/public.h"
+
+class InputCtrlType
+{
+public:
+	int			MouseX, MouseY, MouseEntry;
+	bool		LDown, MDown, RDown;
+	long long	PlayerGuid;
+	double		CameraX, CameraY;
+	InputCtrlType(void)
+	{
+		MouseX = MouseY = MouseEntry = 0;
+		LDown = MDown = RDown = false;
+		PlayerGuid = 0;
+		CameraX = CameraY = 0.0;
+		return ;
+	}
+};
+
+#endif /* NATIVE_INPUTCTRL_H_ */
